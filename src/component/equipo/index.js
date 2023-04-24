7e667bd7ab26b732a50bb41f70997211fb76d677
Juan Morales <juan1810.jm@gmail.com>
2023-04-24 16:52:10 -0300

    dia 24 error que no encuentro

--- src/component/equipo/index.js
+++ src/component/equipo/index.js
@@ -1,14 +1,20 @@
 import "./equipo.css"
+import Colaborador from "../colaborador";
 
 const Equipo = (props)=>{
     // Destructuracion
     const {colorPrimario, colorSecundario,titulo} = props.datos;
+    const {colaboradores} = props;
     const estiloTitulo = {borderColor: colorPrimario}
 
     return <section className="equipo" style={{backgroundColor: colorSecundario}}>
         <h3 style={estiloTitulo}>{titulo}</h3>
         <div className="colaboradores">
-
+            <Colaborador/>
+            {
+                colaboradores.map((colaborador)=> <Colaborador datos={colaborador} key={colaborador.id}/>)
+            }
+            
         </div>
     </section>
 }
